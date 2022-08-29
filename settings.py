0b65adeeaa6a4a17e2e72ac487317a58571fbf8f
Andrey Pavlov <74722708+dronpavlove@users.noreply.github.com>
2022-08-29 21:37:26 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
-TOKEN = "1805539712:AAGg430tDYqVORXm8zi46ipIK5sBT4EHLm0"
+TOKEN = ""
 headers_dict = {
-        'x-rapidapi-key': "7daf8dc2d0mshcbd7d034b2255a3p181d19jsna3eb6b35f28a",
+        'x-rapidapi-key': "",
         'x-rapidapi-host': "hotels4.p.rapidapi.com"
     }
 headers_dict_new = {
-        'x-rapidapi-key': 'ee3675b80emsh30b7faec807b937p137727jsn27ea13765b80',
+        'x-rapidapi-key': '',
         'x-rapidapi-host': 'hotels4.p.rapidapi.com'
 }
